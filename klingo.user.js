8e503c7f5e82f9784b51fbf90efc3ca67d031ac0
mtialison <alisonmateusalvesbarnabe@gmail.com>
2026-04-17 10:40:17 -0300

    Update klingo.user.js

--- klingo.user.js
+++ klingo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         klingo
 // @namespace    http://tampermonkey.net/
-// @version      2.35
+// @version      2.36
 // @description  envenenado
 // @match        *://*.klingo.app/*
 // @match        *://samec.klingo.app/*
